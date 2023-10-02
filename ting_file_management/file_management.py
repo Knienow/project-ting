3859093cfcdb050908a3fc8a3fe9bbda56af5f85
Katia Nienow <nienowkatia@gmail.com>
2023-10-02 16:26:12 -0300

    requisito 02 - ok

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""   
+    if '.txt' not in path_file:
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        file = open(path_file, mode="r")
+        return file.read().split("\n")
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
