5f242cc12944d5953722f2c2d1cee8008f1c0ea6
Katia Nienow <nienowkatia@gmail.com>
2023-10-02 16:45:14 -0300

    requisito 03 - ok

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    files = []
+    for element in instance.queue:
+        files.append(element["nome_do_arquivo"])
+
+    if path_file not in files:
+        text = txt_importer(path_file)
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(text),
+            "linhas_do_arquivo": text
+        }
+    instance.enqueue(data)
+    print(data, file=sys.stdout)
 
 
 def remove(instance):
